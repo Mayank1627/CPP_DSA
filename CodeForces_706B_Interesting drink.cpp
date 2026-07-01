8449047f4f1c4d873622eb584d35f398dbbdb073
Mayank1627 <mayankraoyadav2705@gmail.com>
2026-07-01 12:00:09 +0530

    CodeForces_706B

---
+++ CodeForces_706B_Interesting drink.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n;
+    cin >> n;
+
+    vector<int> prices(n);
+    for (int i = 0; i < n; i++) {
+        cin >> prices[i];
+    }
+
+    sort(prices.begin(), prices.end());
+
+    int q;
+    cin >> q;
+
+    while (q--) {
+        int m;
+        cin >> m;
+
+        int ans = upper_bound(prices.begin(), prices.end(), m) - prices.begin();
+        cout << ans << "\n";
+    }
+
+    return 0;
+}
